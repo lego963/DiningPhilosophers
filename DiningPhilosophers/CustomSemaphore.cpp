02afe0ddb0f8429ae3ecee0059517f469f71046b
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-12-11 23:00:51 +0300

    Добавьте файлы проекта.

---
+++ DiningPhilosophers/CustomSemaphore.cpp
@@ -0,0 +1,65 @@
+﻿#include "CustomSemaphore.h"
+
+/*
+CustomSemaphore implementation
+*/
+
+// Generate a private semaphore
+CustomSemaphore::CustomSemaphore() {
+	zero.sem_num = 0, zero.sem_op = 0, zero.sem_flg = SEM_UNDO;
+	lock.sem_num = 0, lock.sem_op = -1, lock.sem_flg = SEM_UNDO;
+	unlock.sem_num = 0, unlock.sem_op = 1, unlock.sem_flg = SEM_UNDO;
+	my_pid = getpid();
+	if ((semid = semget(IPC_PRIVATE, 1, 0660)) == -1) {
+		exit(1);
+
+	}
+	Put(0);                           // Default - set to zero @ start
+}
+
+// Remove semaphore if creator
+CustomSemaphore::~CustomSemaphore() {
+	if (getpid() == my_pid)
+		if (semctl(semid, 0, IPC_RMID) == -1)
+			exit(2);
+}
+
+// LOCK semaphore
+// Atomic test & decrement
+int CustomSemaphore::P() {
+	if (semop(semid, &lock, 1) == -1)
+		exit(3);
+	return 0;
+
+}
+
+// UNLOCK semaphore
+// Increment semaphore
+void CustomSemaphore::V() {
+	if (semop(semid, &unlock, 1) == -1)
+		exit(4);
+}
+
+// Wait for semaphore to be 0
+int CustomSemaphore::Z() {
+	if (semop(semid, &zero, 1) == -1)
+		exit(5);
+	return 0;
+
+}
+
+// Assign value to the semaphore
+void CustomSemaphore::Put(int const value) {
+	arg.val = value;
+	if (semctl(semid, 0, SETVAL, arg) == -1)
+		exit(6);
+
+}
+
+// Return value of the semaphore
+int CustomSemaphore::Get() {
+	int sem_value;
+	if ((sem_value = semctl(semid, 0, GETVAL, 0)) == -1)
+		exit(7);
+	return sem_value;
+}
---
+++ DiningPhilosophers/DiningPhilosophers.cpp
@@ -0,0 +1,62 @@
+﻿// DiningPhilosophers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*
+The dining philosophers
+*/
+#include <iostream>
+#include "CustomSemaphore.h"                // Our basic semaphore class
+
+const int MAX = 5;
+CustomSemaphore Forks[MAX];
+void Philosopher(int);
+void Eat_It(const int, const int, const int);
+
+
+int main(int argc, char* argv[]) {
+	int i;
+	if (argc < 2) {
+		cerr << "Usage: " << argv[0] << " secs_to_wait " << endl;
+		return 1;
+	}
+
+	for (i = 0; i < MAX; ++i) {
+		Forks[i].Put(true);
+	}
+
+	for (i = 0; i < MAX; ++i) {
+		Philosopher(i);
+	}
+	sleep(atoi(argv[1]));                // Parent process waits a bit
+	return 0;
+
+}
+
+void Philosopher(int number) {
+	if (fork() == 0) {                   // Run in the child
+		int left, right;
+		srand(getpid());
+		left = number;
+		right = (number + 1) % MAX;
+		do {
+			cout << "A. P" << number << " is thinking\n";
+			sleep(rand() % 3 + 1);           // Take a while to THINK
+			cout << "B. P" << number << " ASKS to eat with forks " << left << " & " << right << endl;
+			Forks[left].P();                 // Acquire left fork
+			Forks[right].P();                // Acquire right fork
+			Eat_It(number, left, right);
+			Forks[right].V();
+			Forks[left].V();
+
+		} while (true);
+
+	}
+}
+
+void Eat_It(const int number, const int left, const int right) {
+	cout << "C. P" << number << " is EATING   with forks " << left << " & " << right << endl;
+
+	sleep(rand() % 3 + 1);              // Take a while to EAT
+
+	cout << "D. P" << number << " is now DONE with forks " << left << " & " << right << endl;
+}
